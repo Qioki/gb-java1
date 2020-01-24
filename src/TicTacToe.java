085feba5c52502580b192bde1dbd51c484f65d45
Qioki <8wspace@gmail.com>
2020-01-24 12:51:13 +0300

    L3

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -13,7 +13,6 @@ public class TicTacToe {
     private static int fieldSizeY;
     private static int winnerLineLength;
     private static char[][] field;
-    private static char[][] thinking;
 
     static final int[] vector10 = new int[]{ 1, 0 };
     static final int[] vector11 = new int[]{ 1, 1 };
@@ -27,8 +26,8 @@ public class TicTacToe {
         winnerLineLength = 3;
 
         initField(size);
-        field[0][0] = DOT_HUMAN;
-        field[1][1] = DOT_HUMAN;
+        //field[2][0] = DOT_HUMAN;
+        //field[1][1] = DOT_HUMAN;
         //field[0][2] = DOT_HUMAN;
         printField();
 
@@ -55,22 +54,77 @@ public class TicTacToe {
             }
         }
     }
+    
     private static boolean checkWin(char c) {
-        return checkField(c, winnerLineLength).length != 0;
+        //return checkField(c, winnerLineLength)[3] <= 1;
+        int[] result = checkField(c);
+        return result.length != 0 && result[2] == 0;
     }
-    private static int[] checkField(char c, int lineLength) {
 
+    private static int[] checkField(char c) {
+        int[] temp;
+        int[] bestChoice = new int[]{0, 0, 999};
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                temp = isPossibleHere(c, j, i, winnerLineLength); 
+                if(temp[2] < bestChoice[2])
+                    bestChoice = temp; 
+            }
+        }
+        return bestChoice;
+    }
+
+    private static int[] isPossibleHere(char c, int x, int y, int lineLength) {
+        int[] temp;
+        int[] bestChoice = new int[]{0, 0, 999};
+        for (int i = 0; i < vectors.length; i++) { 
+            temp = isPossibleLine(c, x, y, vectors[i], lineLength); 
+            if(temp.length != 0 && temp[2] < bestChoice[2])
+                bestChoice = temp;
+        }
+        //System.out.println(Arrays.toString(bestChoice));
+        return bestChoice;
+    }
+
+    private static int[] isPossibleLine(char c, int x, int y, int[] vector, int length) {
+        if( (x + vector[0]*length > fieldSizeX) || 
+            (y + vector[1]*length > fieldSizeY) || 
+            (x + vector[0]*length < -1) || 
+            (y + vector[1]*length < -1)) 
+            return new int[0];
+
+        //System.out.println("@@@");
+        int [] emptyHere = new int[3];
+        int emptyCount = 0;
+        for (int i = 0; i < length; i++) {
+            if(field[y][x] == DOT_EMPTY) {
+                emptyHere[0] = y;
+                emptyHere[1] = x;
+                emptyHere[2] = ++emptyCount;
+            }
+            else if(field[y][x] != c) return new int[0];
+            x += vector[0];
+            y += vector[1];
+        }
+        return emptyHere;
+    } 
+
+    private static int[] checkField(char c, int lineLength) {
+        int[] bestChoice = new int[]{0, 0, 0, 999};
         for (int i = 0; i < fieldSizeY; i++) {
             for (int j = 0; j < fieldSizeX; j++) {
                 if(field[j][i] == c) {
                     int[] coo = checkArea(c, i, j, lineLength);
-                    if (coo[4] == 1) {
+                    if (coo[3] == 1) {
                         return coo;
+                    }
+                    if(coo[3] < bestChoice[3]) {
+                        bestChoice = coo;                
                     }
                 }
             }
         }
-        return new int[0];
+        return bestChoice;
     }
     private static int[] checkArea(char c, int x, int y, int lineLength) {
         int minLength = 999;
@@ -85,31 +139,35 @@ public class TicTacToe {
             }
         }
         System.out.println(Arrays.toString(vectors[bestLineId]));
-        return  new int[]{ x, y, vectors[bestLineId][0], vectors[bestLineId][1], lineLength };
-
-        /*return  checkLine(c, x, y, vector10, lineLength) == 1 ||
-                checkLine(c, x, y, vector11, lineLength) == 1 ||
-                checkLine(c, x, y, vector01, lineLength) == 1 ||
-                checkLine(c, x, y, vectorM11, lineLength) == 1;*/
+        System.out.println(minLength);
+        return  new int[]{ x, y, bestLineId, minLength}; 
     }
     private static int checkLine(char c, int x, int y, int[] vector, int length) {
-        if(x >= fieldSizeX || y >= fieldSizeY || x < 0 || y < 0 || field[y][x] != c) return length;
+        if(x >= fieldSizeX || y >= fieldSizeY || x < 0 || y < 0 || field[y][x] != c) return length + 1;
         return length > 1 ? checkLine(c, x+vector[0], y+vector[1], vector, length-1) : 1;
-    }
-
-    private static void aiChecksHuman() {
-
-    }
+    } 
 
     private static void aiTurn() {
-        int x;
-        int y;
+        int caution = 1;  
+        int x = RANDOM.nextInt(fieldSizeX);
+        int y = RANDOM.nextInt(fieldSizeY);
+
+        int[] humanState = checkField(DOT_HUMAN);
+        if(humanState[2] <= caution) {
+            x = humanState[1];
+            y = humanState[0];
+        }
+        int[] aiState = checkField(DOT_HUMAN);
+        if(aiState[2] == 1) {
+            x = aiState[1];
+            y = aiState[0];
+        }
 
 
-        do {
+        while (!isCellEmpty(x, y)) {
             x = RANDOM.nextInt(fieldSizeX);
             y = RANDOM.nextInt(fieldSizeY);
-        } while (!isCellEmpty(x, y));
+        }
 
 
         field[y][x] = DOT_AI;
@@ -140,8 +198,7 @@ public class TicTacToe {
 
     private static void initField(int size) {
         fieldSizeX = fieldSizeY = size;
-        field = new char[fieldSizeY][fieldSizeX];
-        //thinking = new char[fieldSizeY][fieldSizeX];
+        field = new char[fieldSizeY][fieldSizeX]; 
         for (int i = 0; i < fieldSizeY; i++) {
             for (int j = 0; j < fieldSizeX; j++) {
                 field[i][j] = DOT_EMPTY;
